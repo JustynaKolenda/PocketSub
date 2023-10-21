dbcfd337cc42c05ae9655d14514e22468b9f4364
Justyna Kolenda <justynakolenda7@gmail.com>
2023-10-21 23:04:51 +0200

    Develop (#4)
    * add schema
    
    * Setup db (#3)
    
    * Add config for db
    
    * Setup db

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-
 export default function Home() {
-  return (
-    <main className="mx-4 text-center px-0">
-      Hello from PocketSub
-    </main>
-  )
+  return <main className="mx-4 text-center px-0">Hello from PocketSub</main>;
 }
